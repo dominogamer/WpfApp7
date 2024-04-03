92640b039135fa469f88694b7802ed896f28f83a
tarasinski.dawid <tarasinski.dawid@STACJA105-09>
2024-04-03 14:02:04 +0200

    Dodaj pliki projektów.

---
+++ WpfApp7/Nieruchomosc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ZadanieLista
+{
+    class Nieruchomosc
+    {
+        public int Id { get; set; }
+        public string Osiedle { get; set; }
+        public string Adres { get; set; }
+        public bool ZGarazem { get; set; }
+        public Rodzaj Rodzaj { get; set; }
+        public bool Dostepnosc { get; set; }
+        public string Opis { get; set; }
+
+        public Nieruchomosc(int id, string osiedle, string adres, bool zGarazem, Rodzaj rodzaj, bool dostepnosc, string opis)
+        {
+            Id = id;
+            Osiedle = osiedle;
+            Adres = adres;
+            ZGarazem = zGarazem;
+            Rodzaj = rodzaj;
+            Dostepnosc = dostepnosc;
+            Opis = opis;
+        }
+    }
+    public enum Rodzaj
+    {
+        M2,
+        M3,
+        M4
+    }
+
+}
